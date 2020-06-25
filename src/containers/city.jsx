8abe3a8d3d3b8fb220695032040211253aef0b79
benoit barges <benoitbarges@gmail.com>
2020-06-25 09:47:43 +0200

    change background color on click

--- src/containers/city.jsx
+++ src/containers/city.jsx
@@ -6,14 +6,21 @@ import { setActiveCity } from '../actions';
 
 
 class City extends Component {
+
   handleClick = () => {
     this.props.setActiveCity(this.props.city);
   }
 
 
   render() {
+
+    let classes = "list-group-item";
+    if (this.props.city === this.props.activeCity) {
+      classes += " selected";
+    }
+
     return (
-      <div className="list-group-item" onClick={this.handleClick}>
+      <div className={classes} onClick={this.handleClick}>
         {this.props.city.name}
       </div>
     );
@@ -28,4 +35,10 @@ function mapDispatchToProps(dispatch) {
   );
 }
 
-export default connect(null, mapDispatchToProps)(City);
+function mapStateToProps(state) {
+  return {
+    activeCity: state.activeCity
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(City);
